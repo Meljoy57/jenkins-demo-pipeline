264213588611dc79661711e6efce7334a60e3541
Meljoy57 <151118810+Meljoy57@users.noreply.github.com>
2025-05-07 20:44:46 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+def greet(name):
+    return f"Hello, {name}! Welcome to Jenkins Pipelines."
+
+# Example call to the function
+if __name__ == "__main__":
+    message = greet("Meljoy")
+    print(message)
